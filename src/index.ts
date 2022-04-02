2c8f1166f38ec051b4570464ca91a61acea8b1a5
Peïo Thibault <peio.thibault@gmail.com>
2022-04-02 16:22:19 +0200

    Revert "feat: create basic bot (#1)"
    This reverts commit 18051aa1e6b5c86d7960d7330177325dccce47db.

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Intents } from 'discord.js';
-import * as env from 'env-var';
-
-const TOKEN = env.get('DISCORD_TOKEN').required().asString();
-
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-bot.once('ready', () => console.log('Ready!'));
-bot.login(TOKEN);
