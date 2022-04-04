c71b264f37f3bf2cbe297628953e6f4d3099ecd6
neolectron <neolectron@codinglab.io>
2022-04-04 10:47:47 +0200

    feat(featureLoader): load every features in folder with dynamic import.

---
+++ src/featureLoader.ts
@@ -0,0 +1,27 @@
+import type { Client } from 'discord.js';
+import { readdir, stat } from 'fs/promises';
+import { join, resolve } from 'path';
+
+const rootFeatureFolder = './features';
+
+const loadFeatures = async (bot: Client) => {
+  if (!bot) throw new Error('Bot is not defined');
+
+  const featurePaths = await readdir(rootFeatureFolder);
+
+  const features = featurePaths.map(async (featureName) => {
+    const fullPath = join(rootFeatureFolder, featureName);
+    if (!(await stat(fullPath)).isDirectory()) {
+      console.log(`${fullPath} is not a directory. Ignoring.`);
+      return;
+    }
+
+    console.log(`Loading feature ${featureName}`);
+    const { default: feature } = await import(resolve(fullPath));
+    return feature(bot);
+  });
+
+  return Promise.all(features);
+};
+
+export { loadFeatures };
