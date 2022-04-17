97775c631932b1b5061e3fc699b7316f577a1e29
ayoubqrt <moustakim.ayoub@hotmail.com>
2022-04-17 15:49:10 +0200

    feat: add new intent to listen voice state events

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as env from "env-var";
 const main = async () => {
   const TOKEN = env.get("DISCORD_TOKEN").required().asString();
 
-  const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
+  const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] });
 
   bot.once("ready", () => console.log("Ready!"));
 
