f9bfc724da96cd2a35bafad16e11c0712064fcf0
ayoubqrt <moustakim.ayoub00@gmail.com>
2022-04-27 15:31:25 +0200

    feat: add new intent to listen voice state events

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,19 @@ import { Client, Intents } from 'discord.js';
 import * as env from 'env-var';
 import { loadFeatures } from './featureLoader';
 
-const TOKEN = env.get('DISCORD_TOKEN').required().asString();
+const main = async () => {
+  const TOKEN = env.get('DISCORD_TOKEN').required().asString();
 
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
+  const bot = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES],
+  });
 
-bot.once('ready', async () => {
-  console.log(`Logged in as ${bot.user?.tag ?? 'unknown'}!`);
-  const status = await loadFeatures(bot);
-  console.log(`Loaded ${status.length} features.`);
-});
+  bot.once('ready', async () => {
+    console.log(`Logged in as ${bot.user?.tag ?? 'unknown'}!`);
+    const status = await loadFeatures(bot);
+    console.log(`Loaded ${status.length} features.`);
+  });
+  await bot.login(TOKEN);
+};
 
-bot.login(TOKEN);
+void main();
